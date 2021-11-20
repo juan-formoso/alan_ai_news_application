f50406e8b1f6538f74c80fc06571a8179c9f1866
Juan Formoso <trajano.juan@gmail.com>
2021-11-20 19:16:49 -0400

    Add 'talk' function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-const alanKey = '';
+const alanKey = 'ccfa9b86bdc10c97137f17d731f6cd502e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
 
   useEffect(() => {
     alanBtn({
       key: alanKey,
+      onCommand: ({ command }) => {
+        if(command === 'testCommand') {
+          alert('This code was executed');
+        }
+      }
     })
   }, [])
   return(
