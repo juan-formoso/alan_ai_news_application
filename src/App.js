8b5768890c14b5fe3d08d5fcdcf469e28850b6d3
Juan Formoso <trajano.juan@gmail.com>
2022-01-08 20:56:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   useEffect(() => {
     alanBtn({
-      key: '64370f4c903e66c5b517887fefa45c1b2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      key: 'ccfa9b86bdc10c97137f17d731f6cd502e956eca572e1d8b807a3e2338fdd0dc/stage',
       onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
           setNewsArticles(articles);
