b236e8d1817216038ab49ac8125026811ba81f24
Juan Formoso <trajano.juan@gmail.com>
2022-01-14 17:33:11 -0300

    Modidy alt text from logo img

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ const App = () => {
         } else if (command === 'highlight') {
           setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
         } else if (command === 'open') {
-          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const parsedNumber = number.length > 2
+            ? wordsToNumbers(number, { fuzzy: true })
+            : number;
           const article = articles[parsedNumber - 1];
 
           if (parsedNumber > articles.length) {
@@ -46,11 +48,23 @@ const App = () => {
       <div className={classes.logoContainer}>
         {newsArticles.length ? (
           <div className={classes.infoContainer}>
-            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
-            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
           </div>
         ) : null}
-        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+        <img src="" className={classes.alanLogo} alt="NEWS" />
       </div>
       <NewsCards articles={newsArticles} activeArticle={activeArticle} />
       <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
@@ -58,7 +72,13 @@ const App = () => {
         <div className={classes.footer}>
           <Typography variant="body1" component="h2">
             Created by
-            <a className={classes.link} href="https://www.linkedin.com/in/juanvformoso/"> Juan Formoso</a>
+            <a
+              className={classes.link}
+              href="https://www.linkedin.com/in/juanvformoso/"
+            >
+              {' '}
+              Juan Formoso
+            </a>
           </Typography>
         </div>
       ) : null}
